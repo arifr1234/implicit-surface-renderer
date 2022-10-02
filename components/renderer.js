d814475ff59169e0228ddd5f305e2633840eae87
ari <arifr1234@gmail.com>
2022-10-02 20:31:43 +0300

    rename fs

--- components/renderer.js
+++ components/renderer.js
@@ -3,8 +3,8 @@ import React from 'react'
 import * as twgl from 'twgl.js'
 
 import vertex_shader from "../shaders/vs.glsl";
-import fragment_shader from "../shaders/fs.glsl";
-import fragment_shader2 from "../shaders/fs2.glsl";
+import buffer_A_fragment_shader from "../shaders/buffer_A_fs.glsl";
+import image_fragment_shader from "../shaders/image_fs.glsl";
 
 function get_attachments(uniforms){
   return Object.fromEntries(Object.entries(uniforms).map(([key, value]) => [key, value.attachments[0]]));
@@ -44,11 +44,11 @@ export default class Renderer extends React.Component{
     const image = {};
     const A = {};
 
-    image.program = twgl.createProgramInfo(gl, [vertex_shader, fragment_shader2], err => {
+    image.program = twgl.createProgramInfo(gl, [vertex_shader, image_fragment_shader], err => {
       throw Error(err);
     });
 
-    A.program = twgl.createProgramInfo(gl, [vertex_shader, fragment_shader], err => {
+    A.program = twgl.createProgramInfo(gl, [vertex_shader, buffer_A_fragment_shader], err => {
       throw Error(err);
     });
 
