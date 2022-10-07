62d72074c7ddd4ec02b3385f11bd4957daf229a7
ari <arifr1234@gmail.com>
2022-10-07 15:33:16 +0300

    better image shader

--- shaders/image_fs.glsl
+++ shaders/image_fs.glsl
@@ -3,12 +3,17 @@ uniform sampler2D buffer_A;
 
 out vec4 out_color;
 
-vec3 R_gradient(vec3 p, R_params params)
+vec4 R_gradient_and_value(vec3 p, R_params params)
 {
-  return vec3(
-    R(transpose(mat2x3(p, vec3(1, 0, 0))), params).y,
-    R(transpose(mat2x3(p, vec3(0, 1, 0))), params).y,
-    R(transpose(mat2x3(p, vec3(0, 0, 1))), params).y
+  dual dr_dx = R(transpose(mat2x3(p, vec3(1, 0, 0))), params);
+  dual dr_dy = R(transpose(mat2x3(p, vec3(0, 1, 0))), params);
+  dual dr_dz = R(transpose(mat2x3(p, vec3(0, 0, 1))), params);
+
+  return vec4(
+    dr_dx.y,
+    dr_dy.y,
+    dr_dz.y,
+    dr_dx.x  // last value is the value of r.
   );
 }
 
@@ -19,15 +24,18 @@ void main() {
 
   vec3 p = t * scene.ray + scene.camera;
 
-  vec3 gradient = R_gradient(p, R_params(time));
-  gradient = normalize(gradient);
+  vec4 gradient_and_value = R_gradient_and_value(p, R_params(time));
+  vec3 gradient = normalize(gradient_and_value.xyz);
+  float r_value = gradient_and_value.w;
 
   vec3 color = vec3(0);
 
   vec3 light = normalize(vec3(1, 2, 0));
-  color = vec3((dot(gradient, light) + 1.) / 2.); 
+  float shade = (dot(gradient, -light) + 1.) / 2.;
 
-  color = 0.5 * gradient + 0.5;
+  float is_zero = 1. - smoothstep(0., 1., r_value);
+
+  color = shade * vec3(1., is_zero, 1.);
 
   out_color = vec4(color, 1.);
 }
